33dcda80fa897d5323a57171d4b0d14a24c896bf
Srikant Gudi <53587620+srikantgudi@users.noreply.github.com>
2025-02-27 14:35:08 +0530

    added filter option

--- students.cpp
+++ students.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <ncurses.h>
 #include "students.h"
+
+using namespace std;
 
 const int ERR_ROW = 15;
 
@@ -9,19 +12,19 @@ StudentClass::StudentClass() {
     studf = open64(filename.c_str(),O_RDWR | O_CREAT, 0755);
 }
 StudentClass::~StudentClass() {
-    wrefresh(mainw);
+    refresh();
     endwin();
     close(studf);
 }
 int StudentClass::input(StudentRecord &s) {
-    wclear(mainw);
+    clear();
     char rollno[5];
-    wattron(mainw,A_REVERSE|A_BOLD);
-    mvwaddstr(mainw,1,10,"Add New Student");
-    wattroff(mainw,A_REVERSE|A_BOLD);
-    mvwaddstr(mainw,3,10,"Roll No.");
-    mvwaddstr(mainw,3,40,"(0 to exit)");
-    mvwgetstr(mainw,3,30,rollno);
+    attron(A_REVERSE|A_BOLD);
+    mvaddstr(1,10,"Add New Student");
+    attroff(A_REVERSE|A_BOLD);
+    mvaddstr(3,10,"Roll No.");
+    mvaddstr(3,40,"(0 to exit)");
+    mvgetstr(3,30,rollno);
     if (!strlen(rollno)) {
         return RECORD_INVALID;
     } else {
@@ -30,21 +33,24 @@ int StudentClass::input(StudentRecord &s) {
             return RECORD_INVALID;
         }
     }
-    if (isExisting(s)) {
+    if (isExisting(s, "rollno")) {
         return RECORD_EXISTS;
     }
-    mvwaddstr(mainw,5,10,"First name: ");
-    mvwgetstr(mainw,5,30,s.fname);
+    mvaddstr(5,10,"First name: ");
+    mvgetstr(5,30,s.fname);
     if (!strlen(s.fname)) {
         return RECORD_INVALID;
     }
-    mvwaddstr(mainw, 7,10,"Last name: ");
-    mvwgetstr(mainw, 7,30, s.lname);
+    mvaddstr(7,10,"Last name: ");
+    mvgetstr(7,30, s.lname);
     if (!strlen(s.lname)) {
         return RECORD_INVALID;
     }
-    mvwaddstr(mainw,9,10, "Grade: ");
-    mvwgetstr(mainw,9,30, s.grade);
+    if (isExisting(s,"name")) {
+        return RECORD_DUP_NAME;
+    }
+    mvaddstr(9,10, "Grade: ");
+    mvgetstr(9,30, s.grade);
     if (!strlen(s.grade)) {
         return RECORD_INVALID;
     }
@@ -52,30 +58,39 @@ int StudentClass::input(StudentRecord &s) {
     return RECORD_VALID;
 }
 void StudentClass::addRecord() {
-    wclear(mainw);
+    clear();
     int status = input(student);
     if (status == RECORD_VALID) {
-        mvwaddstr(mainw,ERR_ROW,10,"Added new record..");
+        mvaddstr(ERR_ROW,10,"Added new record..");
     } else {
         if (status == RECORD_EXISTS) {
-            mvwaddstr(mainw,ERR_ROW,10,"Record already exists..");
+            mvaddstr(ERR_ROW,10,"Record already exists..");
+        } else if (status == RECORD_DUP_NAME) {
+            mvaddstr(ERR_ROW,10,"Duplicate name encountered..");
         } else if (status == RECORD_INVALID) {
-            mvwaddstr(mainw,ERR_ROW,10,"Invalid input..");
+            mvaddstr(ERR_ROW,10,"Invalid input..");
         }
     }
-    mvwaddstr(mainw,ERR_ROW+1,10,"Press a key to continue..");
-    wrefresh(mainw);
+    mvaddstr(ERR_ROW+1,10,"Press a key to continue..");
+    refresh();
     getch();
-    wclear(mainw);
+    clear();
 }
-bool StudentClass::isExisting(const StudentRecord & s) {
+bool StudentClass::isExisting(const StudentRecord & s, const string & col="rollno") {
     StudentRecord tmp;
     rewind();
     bool found = false;
     while (readFromFile(tmp) && !found) {
-        if (tmp.rollno == s.rollno) {
-            found = true;
-            record_num = lseek64(studf,0,SEEK_CUR)-sizeof(s);
+        if (col == "rollno") {
+            if (tmp.rollno == s.rollno) {
+                found = true;
+                record_num = lseek64(studf,0,SEEK_CUR)-sizeof(s);
+            }
+        } else if (col == "name") {
+            if (!strcmp(tmp.fname, s.fname) && !strcmp(tmp.lname,s.lname)) {
+                found = true;
+                record_num = lseek64(studf,0,SEEK_CUR)-sizeof(s);
+            }
         }
     }
     return found;
@@ -92,39 +107,83 @@ long StudentClass::readFromFile(StudentRecord & s) {
 }
 void StudentClass::showList() {
     rewind();
-    wclear(mainw);
-    wattron(mainw, A_REVERSE|A_BOLD);
-    mvwaddstr(mainw, 1, 5, "Students List");
-    wattroff(mainw, A_REVERSE|A_BOLD);
-    wmove(mainw,3,0);
+    clear();
+    attron(A_REVERSE|A_BOLD);
+    mvaddstr(1, 5, "Students List");
+    attroff(A_REVERSE|A_BOLD);
+    move(3,0);
+    bool show = true;
     while (readFromFile(student) > 0) {
-        wprintw(mainw, "%10d %-30s %-30s %-10s\n", student.rollno, student.fname, student.lname, student.grade);
+        printw("%10d %-30s %-30s %-10s\n", student.rollno, student.fname, student.lname, student.grade);
     }
-    waddstr(mainw, "Press a key to continue..");
-    wgetch(mainw);
+    addstr("Press a key to continue..");
+    getch();
+}
+void  StudentClass::filterList() {
+    char opt;
+    char lname[30], grade[10];
+    StudentRecord st;
+    do {
+        clear();
+        attron(A_REVERSE|A_BOLD);
+        mvprintw(1,20,"Filter List");
+        attroff(A_REVERSE|A_BOLD);
+        mvprintw(2,10,"l. Last name | g. Grade | q. Quit");
+        opt = getch();
+        switch (opt) {
+            case 'l':
+                mvprintw(3,10,"Last name to filter: ");
+                getnstr(lname, 30);
+                rewind();
+                while (readFromFile(st)) {
+                    if (!strcmp(st.lname, lname)) {
+                        printw("%10d | %-30s | %-30s | %-10s\n",st.rollno,st.fname,st.lname,st.grade);
+                    }
+                }
+                addstr("Done!");
+                getch();
+                break;
+            case 'g':
+                mvprintw(3,10,"Grade to filter: ");
+                getnstr(grade, 10);
+                rewind();
+                while (readFromFile(st)) {
+                    if (!strcmp(st.grade, grade)) {
+                        printw("%10d | %-30s | %-30s | %-10s\n",st.rollno,st.fname,st.lname,st.grade);
+                    }
+                }
+                addstr("Done!");
+                getch();
+                break;
+            default:
+                break;
+        }
+    } while (opt != 'q');
 }
 void StudentClass::menu() {
     initscr();
-    mainw = newwin(60,80, 1,1);
-    box(mainw,0,0);
-    keypad(mainw, true);
-    wrefresh(mainw);
+    keypad(stdscr, true);
+    refresh();
     char opt;
     while (opt != 'q') {
-        wclear(mainw);
-        wattron(mainw, A_REVERSE|A_BOLD);
-        mvwaddstr(mainw,2,10,"Students Management System");
-        wattroff(mainw, A_REVERSE|A_BOLD);
-        mvwaddstr(mainw,4,10,"a. Add");
-        mvwaddstr(mainw,5,10,"l. List");
-        mvwaddstr(mainw,6,10,"q. Quit");
-        mvwaddstr(mainw,8,10,"?");
-        wrefresh(mainw);
-        opt = wgetch(mainw);
-        if (opt == 'a') {
-            addRecord();
-        } else if (opt == 'l') {
-            showList();
+        clear();
+        attron(A_REVERSE|A_BOLD);
+        mvaddstr(2,10,"Students Management System");
+        attroff(A_REVERSE|A_BOLD);
+        mvaddstr(4,10,"a. Add");
+        mvaddstr(5,10,"l. List");
+        mvaddstr(6,10,"f. Filter");
+        mvaddstr(7,10,"q. Quit");
+        mvaddstr(8,10,"?");
+        refresh();
+        opt = getch();
+        switch (opt) {
+            case 'a':
+                addRecord(); break;
+            case 'l':
+                showList(); break;
+            case 'f':
+                filterList(); break;
         }
     }
     cout << endl << "Thank you for using the app" << endl;
--- students.h
+++ students.h
@@ -1,6 +1,7 @@
 #ifndef __STUDENTS_H
 #define __STUDENTS_H
 
+#include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
@@ -11,6 +12,7 @@ using namespace std;
 const int RECORD_VALID = 0;
 const int RECORD_EXISTS = 1;
 const int RECORD_INVALID = 2;
+const int RECORD_DUP_NAME = 3;
 
 struct StudentRecord {
     int rollno;
@@ -23,18 +25,18 @@ class StudentClass {
     StudentRecord student;
     int studf;
     string filename;
-    WINDOW *mainw;
     long record_num;
 public:
     StudentClass();
     ~StudentClass();
     int input(StudentRecord &s);
     void addRecord();
-    bool isExisting(const StudentRecord & s);
+    bool isExisting(const StudentRecord & s, const string & str);
     void writeToFile(const StudentRecord & s);
     void rewind();
     long readFromFile(StudentRecord & s);
     void showList();
+    void filterList();
     void menu();
 };
 #endif
